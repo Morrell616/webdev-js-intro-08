438b77e941c6615cdaa6eb161010cb7d07914946
Jen Morrell <morrell616@hotmail.com>
2025-01-13 21:54:16 -0500

    added find smallest, largest, and average of (myNumbers)

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,49 @@ const largestNumberElement = document.getElementById("largest-number");
 const averageNumberElement = document.getElementById("average-number");
 
 const myNumbers = [35, 130, 354, 100, 25, 134, 99, 200, 35];
+
+function findSmallestNumber() {
+    let smallestNumber;
+    for (let i =0; i < myNumbers.length; i++) {
+        if (i === 0) {
+            smallestNumber = myNumbers[i];
+      } else {
+            if (smallestNumber > myNumbers[i]) {
+                smallestNumber = myNumbers[i]
+            }
+      }
+    }
+    return smallestNumber;
+}  
+
+function findLargestNumber() {
+    let largestNumber;
+    for (let i = 0; i < myNumbers.length; i++) {
+        if (i === 0) {
+            largestNumber = myNumbers[i];
+      } else {
+            if (largestNumber < myNumbers[i]) {
+                largestNumber = myNumbers[i]
+            }
+      }
+    }
+    return largestNumber;
+}
+
+function findAverage() {
+    let sum = 0;
+    for (let i = 0; i < myNumbers.length; i++) {
+        sum = sum + myNumbers[i];
+    }
+    let average = sum / myNumbers.length;
+    return average;
+}
+function render () {
+   smallestNumberElement.textContent = findSmallestNumber(myNumbers);// text content links back to above and html
+   largestNumberElement.textContent = findLargestNumber(myNumbers);
+   averageNumberElement.textContent = findAverage(myNumbers);
+ }
+ submissionBtn.addEventListener("click", function () {
+     // Call the render function
+     render();
+ });
--- js/index.js
+++ js/index.js
@@ -1,3 +1,52 @@
+/*
+
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+let grades = [80, 92, 100, 67, 90];
+let sum = 0;
+for (let i = 0; i< grades.length; i++) {
+    sum = sum + grades[i];
+}
+// or you can use a for...of
+let numbers = [10, 20, 30, 40, 50];
+for (let number of numbers) {
+    console.log(number);
+
+let average = sum / grades.length;
+console.log(average);
+
+//here is same code with for of
+let grades = [85, 90, 78, 92, 88];
+let sum = 0;
+for (let grade of grades) {
+    sum += grade;
+}
+
+let average = sum / grades.length;
+console.log(average);
+[3, 5, 2, 9, 7, 3];
+let total = 0;
+for (let i = 0; i<numbers.length; i++) {
+    total = total + numbers[i];
+}
+    let answer = total / numbers.length;
+    console.log(answer);
+
+let tens = [10, 20, 30, 40, 50, 60]
+for (let i= 0; i< tens.length; i++)  {
+    tens[i] = tens[i] + 10;
+console.log(tens);
+}
+const numbers = [1, 2, 3, 4, 5];
+numbers.forEach(function(number) {
+    console.log(number);
+    // this creates a lof of each number in the array.
+});
+
+const fruits = ['apple', 'banana', 'cherry'];
+fruits.forEach(function(fruit, index) {
+    console.log(index + ': ' + fruit);
+    // returns the number it is in the array and the type of fruit
+});
+*/
